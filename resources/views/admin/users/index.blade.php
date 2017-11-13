30bf977c29033a59ee0c627bde4382b3be4e356c
Joshua Altmann <josh.alt99@gmail.com>
2017-11-13 09:36:05 -0800

    users section working 100 precent -except security

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -11,13 +11,14 @@
         <thead>
           <tr>
             <th>Id</th>
+            <th>Photo</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
             <th>Status</th>
             <th>Created</th>
             <th>Updated</th>
-            <th>Photo</th>
+
           </tr>
         </thead>
         <tbody>
@@ -28,13 +29,14 @@
 
               <tr>
                     <td> {{$user->id}} </td>
-                    <td> {{$user->name}} </td>
+                    <td> <img height="50" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt="" >  </td>
+                    <td><a href="{{route('admin.users.edit', $user->id)}}"> {{$user->name}} </a> </td>
                     <td> {{$user->email}} </td>
                     <td> {{$user->role->name}} </td>
                     <td> {{$user->is_active ==  1? 'Active': 'Not Active' }} </td>
                     <td> {{$user->created_at->diffForHumans()}} </td>
                     <td> {{$user->updated_at->diffForHumans()}} </td>
-                    <td> {{$user->photo_id}} </td>
+
               </tr>
 
           @endforeach
